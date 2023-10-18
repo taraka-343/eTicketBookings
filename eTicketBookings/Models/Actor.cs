5b9fed837cf4adab5151275a19183f06014cd87d
taraka siva kumar uppu <you@example.com>
2023-10-18 18:19:37 +0530

    Added All Classes

---
+++ eTicketBookings/Models/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicketBookings.Models
+{
+    public class Actor
+    {
+        public int id { get; set; }
+        public string profilePictureUrl { get; set; }
+        public string fullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
